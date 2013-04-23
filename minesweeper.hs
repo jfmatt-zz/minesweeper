f0f99166b2bbd7cf45b0a64d9484c35e9f4c6bae
James Matthews <jmatthews@devscapedesigns.com>
2013-04-23 14:27:09 -0400

    cleanup per hpaste

--- minesweeper.hs
+++ minesweeper.hs
@@ -23,7 +23,7 @@ instance Show Tile where
   show Tile { value = (Touching x) } = " " ++ show x ++ " "
 
 newTile :: Tile
-newTile = Tile { value=(Touching 0), marked=False, hidden=True }
+newTile = Tile { value=Touching 0, marked=False, hidden=True }
 
 newBomb :: Tile
 newBomb = Tile { value=Bomb, marked=False, hidden=True }
@@ -41,7 +41,7 @@ isBomb _ 	= False
 -- Used for flooding the 0 tiles when one is hit
 -- Given a neighbor, is it a 0 that's already been revealed?
 isZero :: Maybe Tile -> Bool
-isZero (Just t) = (value t == Touching 0) && (hidden t == False)
+isZero (Just t) = (value t == Touching 0) && not (hidden t)
 isZero _	= False
 
 --
@@ -57,25 +57,25 @@ data Board = Board {
 instance Show Board where
   show b = showHeader (width b) ++ showTiles (tiles b) 0 where
     align3 n
-      | n < 10   = " " ++ show n ++ " "
+      | n < 10   = ' ' : show n ++ " "
       | n >= 100 = error "Number is too wide to fit here..."
-      | n >= 10  = " " ++ show n
-    showHeader n = "    " ++ (foldl' (\a b -> a ++ (align3 b)) "" [0..n-1]) ++ "\n    " ++ (foldl' (\a b -> a ++ " v ") "" [0..n-1]) ++ "\n"
+      | n >= 10  = ' ' : show n
+    showHeader n = "    " ++ foldl' (\a b -> a ++ align3 b) "" [0..n-1] ++ "\n    " ++ foldl' (\a b -> a ++ " v ") "" [0..n-1] ++ "\n"
     
     showTiles [] _ = ""
-    showTiles ts n = (foldl' (++) (align3 n ++ ">") $ map show thisRow) ++ "\n" ++ showTiles otherRows (n+1) where
+    showTiles ts n = foldl' (++) (align3 n ++ ">") (map show thisRow) ++ "\n" ++ showTiles otherRows (n+1) where
       (thisRow, otherRows) = splitAt (width b) ts
 
 boardGet2 :: Board -> Int -> Int -> Maybe Tile
 boardGet2 b y x 
-  | y >= 0 && x >= 0 && y < height b && x < width b  = Just $ (tiles b) !! yxToN y x b
+  | y >= 0 && x >= 0 && y < height b && x < width b  = Just $ tiles b !! yxToN y x b
   | otherwise					     = Nothing
 
 numTiles :: Board -> Int
-numTiles b = (width b) * (height b)
+numTiles b = width b * (height b)
 
 yxToN :: Int -> Int -> Board -> Int
-yxToN y x b = ((y * width b) + x)
+yxToN y x b = (y * width b) + x
 
 --
 -- Construction
@@ -99,9 +99,9 @@ placeBombs b n g
                       -- Reservoir polling
                       place t      _   0 = t
                       place (t:ts) rng n
-                         | rngValue <= ((fromIntegral n) / (fromIntegral (length (t:ts)))) 	= newBomb : (place ts rng' (n - 1))
-                         | otherwise								= t : (place ts rng' n)
-                         where (rngValue, rng') = (randomR (0, 1) rng) :: (Double, StdGen)
+                         | rngValue <= fromIntegral n / fromIntegral (length (t:ts)) 	= newBomb : place ts rng' (n - 1)
+                         | otherwise							= t : place ts rng' n
+                         where (rngValue, rng') = randomR (0, 1) rng :: (Double, StdGen)
                          
                       -- DOOO EEEEEEIT
                       t' = place (tiles b) g n
@@ -118,11 +118,11 @@ setTouching b = let
                  nextCoord x y
                    | x == w - 1 = (0, y + 1)
                    | otherwise  = (x + 1, y)
-                 getNeighbor x y deltas = boardGet2 b (y + (fst deltas)) (x + (snd deltas))
+                 getNeighbor x y deltas = boardGet2 b (y + fst deltas) (x + snd deltas)
                  
                  -- Add to this tile the # of neighbors who are bombs
                  process []     _ _ = []
-                 process (t:ts) x y = (addTile t $ length . filter isBomb $ map (getNeighbor x y) neighborMoves) : process ts x' y' where
+                 process (t:ts) x y = addTile t (length . filter isBomb $ map (getNeighbor x y) neighborMoves) : process ts x' y' where
                    (x', y') = nextCoord x y
                  
                  t' = process (tiles b) 0 0
@@ -137,20 +137,20 @@ buildBoard w h b rng = setTouching $ placeBombs (blankBoard w h) b rng
 --
 
 markTile :: Tile -> Tile
-markTile Tile v m h  = Tile v (not m) h
+markTile (Tile v m h) = Tile v (not m) h
 
 revealTile :: Tile -> Tile
-revealTile Tile v m _ = Tile v m False
+revealTile (Tile v m _) = Tile v m False
 
 liftBoardN :: (Tile -> Tile) -> Board -> Int -> Board
 liftBoardN f (Board t h w) n = Board (l' f t n) h w where
   l' _ []     _ = []
-  l' f (t:ts) 0 = (f t):ts
-  l' f (t:ts) n = t:(l' f ts (n-1))
+  l' f (t:ts) 0 = f t:ts
+  l' f (t:ts) n = t:l' f ts (n-1)
 
 makeMove :: String -> Board -> Int -> Int -> Board
-makeMove s b y x = (liftBoardN f) b (yxToN y x b) where
-  f = case (s) of
+makeMove s b y x = liftBoardN f b (yxToN y x b) where
+  f = case s of
     "m" -> markTile
     "d" -> revealTile
     _   -> id
@@ -165,11 +165,11 @@ floodZeroHelper oldb b
     nextCoord x y
       | x == w - 1 = (0, y + 1)
       | otherwise  = (x + 1, y)
-    getNeighbor x y deltas = boardGet2 b (y + (fst deltas)) (x + (snd deltas))
+    getNeighbor x y deltas = boardGet2 b (y + fst deltas) (x + snd deltas)
         
     -- Unhide tile if # of neighbors who are revealed 0s is >0
     process []                               _ _                           = []
-    process ((Tile (Touching n) mkd hdn):ts) x y
+    process (Tile (Touching n) mkd hdn:ts) x y
       | (length . filter isZero $ map (getNeighbor x y) neighborMoves) > 0 = Tile (Touching n) mkd False : process ts x' y' where
           (x', y') = nextCoord x y
     process (t:ts)                           x y                           = t                           : process ts x' y' where
@@ -210,18 +210,18 @@ runGame :: Board -> IO ()
 runGame b =
   case (getGameState b) of
     Loss -> do
-    	putStrLn $ show b
-    	putStrLn "You lose."
+    	print  b
+    	print "You lose."
     Win -> do
-    	putStrLn $ show b
-        putStrLn "You win!"
+    	print b
+        print "You win!"
     Continue -> do
-	putStrLn $ show b
-        putStrLn "Next turn: Mark/unmark (m) or dig (d)?"
+	print b
+        print "Next turn: Mark/unmark (m) or dig (d)?"
         m <- getLine
-        putStrLn "Which row?"
+        print "Which row?"
         y <- getLine
-        putStrLn "Which column?"
+        print "Which column?"
         x <- getLine
 	let b' = floodZeros $ makeMove m b (read y) (read x) 
   	runGame b'
@@ -229,11 +229,11 @@ runGame b =
 main :: IO ()
 main = do
   -- Setup
-  putStrLn "Height of board?"
+  print "Height of board?"
   height <- getLine
-  putStrLn "Width of board?"
+  print "Width of board?"
   width <- getLine
-  putStrLn "Number of bombs?"
+  print "Number of bombs?"
   bombs <- getLine
   rng <- newStdGen
   
